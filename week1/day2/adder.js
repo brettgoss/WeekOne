85904f45358629444875908d5ea1f4771a5db393
Brett Goss <brettgoss@live.com>
2016-10-04 19:03:10 +0000

    added dice-roller.js

--- week1/day2/adder.js
+++ week1/day2/adder.js
@@ -1,15 +1,18 @@
 var myArg = process.argv.slice(2);
-var total = 0;
 var number = 0;
 //console.log(myArg);
 
+function sum(numbers) {
+  var total = 0;
 
-for (var i = 0; i < myArg.length; i++) {
-  console.log(i);
-  debugger;
-  var number = Number(myArg[i]);
-  if (isNaN(number) === false) {
-    total += number;
-  } else {}
+  for (var i = 0; i < myArg.length; i++) {
+   console.log(i);
+    debugger;
+    var number = Number(myArg[i]);
+    if (isNaN(number) === false) {
+      total += number;
+    } else {}
+  }
+  console.log(number);
 }
-console.log(total)
+sum(1,2,3,4,5,6);
