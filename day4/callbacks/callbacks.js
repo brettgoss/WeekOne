8a42c20d72d43ce2eb3cfe6c63ebe66eae740064
Brett Goss <brettgoss@live.com>
2016-10-06 20:40:23 +0000

    refactored callback.js to include forEach

--- day4/callbacks/callbacks.js
+++ day4/callbacks/callbacks.js
@@ -1,10 +1,12 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "Waldo") {
-      found(i);   // execute callback
+  var count = 0;
+  arr.forEach(function(i) {
+    if (i == "Waldo") {
+      found(count);   // execute callback
     }
-  }
+    count += 1;
+  })
 }
 
 function actionWhenFound(index) {
